db9a7b3720fd02f860b07286c374722bf6b10949
Lucas DiCioccio <lucas@dicioccio.fr>
2022-02-02 22:38:02 +0100

    add tips and readings pages

--- js/autoreload.js
+++ js/autoreload.js
@@ -2,7 +2,10 @@
   const go = (n) => () => {
     const m = n < 5 ? 1+n : 5;
     const retry = () => setTimeout(go(m), n*1000);
-    fetch("/dev/watch")
+    const url = new URL(document.location.origin + '/dev/watch');
+    const params = [['pathname', document.location.pathname]];
+    url.search = new URLSearchParams(params).toString();
+    fetch(url)
     .then(response => response.json())
     .then(data => {
       if (data === "Reloaded") { 
