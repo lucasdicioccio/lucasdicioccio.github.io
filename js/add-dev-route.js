0ca52e0ddbac95d53e501e2cfed5f163e287fb42
Lucas DiCioccio <lucas@dicioccio.fr>
2022-03-16 00:36:19 +0100

    progress on article

---
+++ js/add-dev-route.js
@@ -0,0 +1,25 @@
+(function(){
+  const callProduce = (ev) => {
+    const btn = ev.target;
+    btn.disabled = true;
+    const url = new URL(document.location.origin + '/dev/produce');
+    fetch(url, {'method': 'POST'})
+    .then(response => response.json())
+    .then(data => {
+      alert("built");
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert("failed");
+    })
+    btn.disabled = false;
+  };
+  const go = () => {
+    let btn = document.createElement('button');
+    btn.className = "dev-produce-button";
+    btn.onclick = callProduce;
+    btn.appendChild(document.createTextNode("produce"));
+    document.body.appendChild(btn);
+  };
+  setTimeout(go, 1000);
+})();
