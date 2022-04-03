be3d80cca87aed9c7b23a8da605c2a5c455e0c8f
Lucas DiCioccio <lucas@dicioccio.fr>
2022-04-03 11:00:55 +0200

    add and explain histogram

--- js/echart-histogram.js
+++ js/echart-histogram.js
@@ -10,6 +10,31 @@ const jsonSrc = document.getElementsByName('ks:article_json')[0].content;
 myChart.showLoading();
 $.get(jsonSrc, function (articlejson) {
   myChart.hideLoading();
+  const slitems = articlejson.skyline.skylineItems;
+  const newSeries = (name) => { return { name: name, type: 'bar', stack: 'one', data: new Array(slitems.length) } };
+  let series = [newSeries('total'), newSeries('incipit')];
+  const offsetSeries = series[0];
+  let currentSeries = series[1];
+  let cumulativeSum = 0;
+  slitems.forEach((item,idx) => {
+    offsetSeries.data[idx] = cumulativeSum;
+    if (item.tag == 'HeaderMark') {
+      const title = item.contents[0];
+      const heading = item.contents[1].reduce((c,x) => `${x}.${c}`, '');
+      const s = newSeries(`${heading} ${title}`);
+      series.push(s);
+      currentSeries = s;
+    }
+    if (item.tag == 'TextualMark') {
+      const val = item.contents[0];
+      cumulativeSum += val;
+      currentSeries.data[idx] = val;
+    }
+  });
+
+  offsetSeries.itemStyle = {'borderColor': 'transparent', color: 'transparent'};
+  offsetSeries.emphasis = {'itemStyle':  {'borderColor': 'transparent', color: 'transparent'}};
+
   option = {
     title: {
       text: 'article histogram',
@@ -17,20 +42,20 @@ $.get(jsonSrc, function (articlejson) {
       top: 'bottom',
       left: 'right'
     },
-    tooltip: {},
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {type: 'shadow'},
+    },
     xAxis: {
-        data: articlejson.histogram.map((_,i) => i),
+        name: 'cmark chunk',
+        data: slitems.map((_,i) => i),
+        silent: true,
     },
     yAxis: {
+      name: 'total words',
       type: 'value'
     },
-    series: [
-      {
-        name: 'words',
-        type: 'bar',
-        data: articlejson.histogram,
-      }
-    ]
+    series: series,
   };
   myChart.setOption(option);
 });
