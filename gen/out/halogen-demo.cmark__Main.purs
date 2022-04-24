a41ef5bc714851950637633e1942c62049a884ab
Lucas DiCioccio <lucas@dicioccio.fr>
2022-04-24 23:17:38 +0200

    publish halogen demo

--- gen/out/halogen-demo.cmark__Main.purs
+++ gen/out/halogen-demo.cmark__Main.purs
@@ -5,9 +5,11 @@ import Prelude
 import Affjax as AX
 import Affjax.ResponseFormat as ResponseFormat
 import Data.Argonaut.Core as J
+import Data.Array as Array
 import Data.Codec.Argonaut as CA
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..),fromMaybe)
+import Data.String (trim, split, contains, Pattern(..))
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Aff.Class (class MonadAff)
@@ -15,6 +17,7 @@ import Effect.Class.Console (log)
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Halogen.VDom.Driver (runUI)
 import Web.DOM.ParentNode (QuerySelector(..))
@@ -48,7 +51,8 @@ main = HA.runHalogenAff do
   let tgt = fromMaybe body elem
   runUI component {routes: fromMaybe [] routes} tgt
 
-type Action = Unit
+data Action
+  = SetFilterString String
 
 data Query a = Query a
 
@@ -60,7 +64,16 @@ type Output = Unit
 
 type State =
   { routes :: Array String
+  , filter :: Maybe String
   }
+
+matchFilter :: String -> String -> Boolean
+matchFilter filter str =
+  Array.all matchString
+  $ split (Pattern " ")
+  $ trim filter
+  where
+    matchString q = Pattern q `contains` str
 
 component
   :: forall m. MonadAff m
@@ -70,14 +83,25 @@ component =
     { initialState
     , render
     , eval: H.mkEval $ H.defaultEval
+      { handleAction = handleAction
+      }
     }
   where
 
-  initialState input = { routes: input.routes }
+  initialState input = { routes: input.routes , filter: "" }
 
   render state =
     HH.div_
-      [ renderRoutes state.routes
+      [ renderFilter state.filter
+      , renderRoutes (Array.filter (matchFilter state.filter) state.routes)
+      ]
+
+  renderFilter str =
+      HH.input
+      [ HP.class_ (HH.ClassName "routes-filter-input")
+      , HE.onValueInput (\e -> SetFilterString e)
+      , HP.value str
+      , HP.placeholder "search"
       ]
 
   renderRoutes rts =
@@ -98,3 +122,6 @@ component =
         [ HH.text r
         ]
       ]
+
+  handleAction = case _ of
+    SetFilterString str -> H.modify_ _ { filter = str } 
