a38c16dca63012844baace75119a894d48f82ad2
Lucas DiCioccio <lucas@dicioccio.fr>
2022-05-02 12:47:03 +0200

    focus on graph-selected items

--- js/site-graph.js
+++ js/site-graph.js
@@ -5,94 +5,149 @@ var chartDom = document.getElementById('echartzone');
 var myChart = echarts.init(chartDom);
 var option;
 
-myChart.showLoading();
-$.get(ROOT_PATH + '/json/topicsgraph.json', function (topicsgraph) {
-  const categoryIndices = ["ArticleNode", "TopicNode", "ImageNode"];
-  const sizeFor = (node) => {
-    switch (node[1]?.tag) {
-      case "ArticleNode":
-        return 10 + Math.log(node[1].contents[1]) / Math.log(2);
-      case "TopicNode":
-        return 10;
-      default:
-        return 5;
-    }
-  };
-  const indexArticleRef = "article:/index.html";
-  const graph = {
-    categories: [{"name":"articles"},{"name":"topics"},{"name":"images"}],
-    links: topicsgraph.edges
-      .filter((e) => { return e[0] !== indexArticleRef && e[1] !== indexArticleRef })
-      .map((e) => { return {source:e[0], target:e[1]} }),
-    nodes: topicsgraph.nodes
-      .filter((n) => { return n[0] !== indexArticleRef })
-      .map((n) => { return {
-        id: n[0]
-      , name: n[0]
-      , symbolSize: sizeFor(n)
-      , category: categoryIndices.findIndex((t) => t == n[1].tag)
-      } 
-    }),
-  }
+const categoryIndices = ["ArticleNode", "TopicNode", "ImageNode"];
 
-  myChart.hideLoading();
-  option = {
-    title: {
-      top: 'bottom',
-      left: 'right'
-    },
-    tooltip: {},
-    legend: [
-      {
-        // selectedMode: 'single',
-        data: graph.categories.map(function (a) {
-          return a.name;
-        })
-      }
-    ],
-    series: [
-      {
-        name: 'blog',
-        type: 'graph',
-        layout: 'force',
-        data: graph.nodes,
-        links: graph.links,
-        categories: graph.categories,
-        roam: true,
-        label: {
-          position: 'right'
-        },
-        force: {
-          repulsion: 100
-        }
-      }
-    ]
-  };
-  myChart.setOption(option);
+const indexArticleRef = "article:/index.html";
 
-  myChart.on('click', function(params) {
-    if (params.componentType === 'series') {
-      if (params.dataType === 'node') {
-	const n = topicsgraph.nodes[params.dataIndex];
+const urlForNode = (n) => {
 	switch (n[1].tag) {
           case "ArticleNode": {
-	    const url = n[1].contents[0];
-            window.open(url, '_blank');
-            break;
+	    return n[1].contents[0];
           }
           case "ImageNode": {
-	    const url = n[1].contents;
-            window.open(url, '_blank');
-            break;
+	    return n[1].contents;
 	  }
           case "TopicNode": {
 	    const topic = n[0].split(':',2)[1];
 	    const urlTopic = topic.replace(/\s/g, '-');
-            const url = `/topics/${urlTopic}.html`
-            window.open(url, '_blank');
-            break;
+            return `/topics/${urlTopic}.html`
 	  }
 	}
+}
+
+const sizeForNode = (n, selection, edges) => {
+  let neighborSelected = false;
+  if (selection.node !== null) {
+    if (edges.findIndex((e) => { return (e[0] === n[0] || e[1] === n[0]) && (e[0] === selection.node[0] || e[1] === selection.node[0]) }) > 0) {
+      neighborSelected = true;
+    }
+  }
+
+  if (neighborSelected) {
+      if (selection.node === n) {
+        return 35;
+      }
+      return 30;
+  }
+
+  switch (n[1]?.tag) {
+    case "ArticleNode":
+      return 10 + Math.log(n[1].contents[1]) / Math.log(2);
+    case "TopicNode":
+      return 10;
+    default:
+      return 5;
+  }
+};
+
+const symbolForNode = (n, selection, edges) => {
+  let neighborSelected = false;
+  if (selection.node !== null) {
+    if (edges.findIndex((e) => { return (e[0] === n[0] || e[1] === n[0]) && (e[0] === selection.node[0] || e[1] === selection.node[0]) }) > 0) {
+      neighborSelected = true;
+    }
+  }
+
+  switch (n[1]?.tag) {
+    case "ArticleNode":
+      return 'rect';
+    case "TopicNode":
+      return 'diamond';
+    case "ImageNode":
+      if (neighborSelected) {
+        return `image://${urlForNode(n)}`;
+      }
+      return 'circle';
+    default:
+      return 'none';
+  }
+};
+
+
+myChart.showLoading();
+$.get(ROOT_PATH + '/json/topicsgraph.json', function (topicsgraph) {
+  const nodes = topicsgraph.nodes
+        .filter((n) => { return n[0] !== indexArticleRef });
+  const edges = topicsgraph.edges;
+  myChart.hideLoading();
+  let selection = {node: null, idx: -1};
+
+  const run = () => {
+    const graph = {
+      categories: [{"name":"articles"},{"name":"topics"},{"name":"images"}],
+      links: edges
+        .filter((e) => { return e[0] !== indexArticleRef && e[1] !== indexArticleRef })
+        .map((e) => { return {source:e[0], target:e[1]} }),
+      nodes: nodes
+        .map((n) => { return {
+          id: n[0]
+        , name: n[0]
+        , symbol: symbolForNode(n, selection, edges)
+        , symbolSize: sizeForNode(n, selection, edges)
+        , category: categoryIndices.findIndex((t) => t == n[1].tag)
+        } 
+      }),
+    }
+  
+    option = {
+      title: {
+        top: 'bottom',
+        left: 'right'
+      },
+      tooltip: {},
+      legend: [
+        {
+          data: graph.categories.map(function (a) {
+            return a.name;
+          })
+        }
+      ],
+      series: [
+        {
+          name: 'blog',
+          type: 'graph',
+          layout: 'force',
+          data: graph.nodes,
+          links: graph.links,
+          categories: graph.categories,
+          roam: true,
+          draggable: true,
+          label: {
+            position: 'right'
+          },
+          force: {
+            repulsion: 100
+          }
+        }
+      ]
+    };
+    myChart.setOption(option);
+  }
+  run();
+
+  myChart.on('click', function(params) {
+    if (params.componentType === 'series') {
+      if (params.dataType === 'node') {
+        const idx = params.dataIndex;
+	const n = nodes[idx];
+        if (selection.node === n) {
+          const url = urlForNode(n);
+          window.open(url, '_blank');
+        } else {
+          selection = {node: n, idx: idx};
+          console.log(selection);
+          run();
+        }
       }
     }
   });
